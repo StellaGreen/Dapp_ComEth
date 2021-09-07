143457ccd961fc357d94175f178aa28549cdb665
StellaGreen <vickysoler5@gmail.com>
2021-09-07 23:07:59 +0200

    front body

--- src/components/organisms/HomeBody.js
+++ src/components/organisms/HomeBody.js
@@ -7,7 +7,7 @@ import VoteButton from "../atoms/VoteButton";
 const HomeBody = () => {
   return (
     <>
-      <SimpleGrid columns={[1, 1, 2, 3]} spacing="40px" m="4rem" ml="12rem">
+      <SimpleGrid columns={[1, 1, 2, 3]} spacing="40px" mr="2rem" mt="4rem" ml={{sm:"9rem", md:"10rem", lg:"12rem"}}>
         <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"20rem", md:"22rem", lg:"29rem"}}>
           <Box boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{sm:"9rem", md:"9rem", lg:"17rem"}}>
            Venez voter les propositions déjà suggérer par votre groupe ComEth
--- src/components/organisms/InformationBody.js
+++ src/components/organisms/InformationBody.js
@@ -8,7 +8,7 @@ const InformationBody = () => {
   return (
     <>
     
-    <SimpleGrid columns={[1, 1, 2, 3]} spacing="40px" m="4rem" ml="12rem">
+    <SimpleGrid columns={[1, 1, 2, 3]} spacing="40px" mr="2rem" mt="4rem" ml={{sm:"9rem", md:"10rem", lg:"12rem"}}>
     <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"20rem", md:"22rem", lg:"29rem"}}>
     <Box boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{sm:"9rem", md:"9rem", lg:"17rem"}}>
            Ici sont les proposition déjà voter avec leurs paramettres
