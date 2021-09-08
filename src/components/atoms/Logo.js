9cb33f759188fd77acceaa3b67f881aa4c9489c6
StellaGreen <vickysoler5@gmail.com>
2021-09-08 14:38:07 +0200

    font bold

--- src/components/atoms/Logo.js
+++ src/components/atoms/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <>
       <Center>
-        <Circle backgroundColor="whiteAlpha.200" w="5rem" h="5rem" textColor="whiteAlpha.400">
+        <Circle  fontWeight="bold" backgroundColor="whiteAlpha.200" w="5rem" h="5rem" textColor="whiteAlpha.400">
           <Link to="/home">Logo</Link>
         </Circle>
       </Center>
--- src/components/templates/StatsTemplate.js
+++ src/components/templates/StatsTemplate.js
@@ -29,11 +29,11 @@ const StatsTemplate = () => {
       </Center>
       <Box backgroundColor="blackAlpha.200" ml={{sm:"9rem",md:"12rem",lg:"16rem"}} mr={{sm:"1rem"}} rounded="md" mt="4rem" p="2rem" width={{sm:"70%",md:"45%",lg:"30%"}} h="55%">
         <Center><Box p="7%" rounded="md" backgroundColor="teal.400" mb="6%" fontWeight="bold" textAlign="center">Dernière proposition voter/ou à voter</Box></Center>
-        <Box mt="3rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">option : {proposal.option}</Box>
-        <Box mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">title : {proposal.title}</Box>
-        <Box mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">time limite : {proposal.timeLimit}</Box>
-        <Box mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">paiment receiver : {proposal.receiver}</Box>
-        <Box mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">amount : {proposal.amount}</Box>
+        <Box p="1%" mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">option : {proposal.option}</Box>
+        <Box p="1%" mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">title : {proposal.title}</Box>
+        <Box p="1%" mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">time limite : {proposal.timeLimit}</Box>
+        <Box p="1%" mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">paiment receiver : {proposal.receiver}</Box>
+        <Box p="1%" mt="2rem" ml={{md:"2rem"}} backgroundColor="teal.400" rounded="lg" fontWeight="bold" onChange={handleProposal} mb="6%">amount : {proposal.amount}</Box>
       </Box>
     </>
   );
