dfd8a629551ed94beffcaa83f3b5db439df1cdaa
StellaGreen <vickysoler5@gmail.com>
2021-09-12 16:58:49 +0200

    pay ok home body front add

--- src/components/atoms/AddUserAndPay.js
+++ src/components/atoms/AddUserAndPay.js
@@ -1,23 +1,59 @@
 import React, { useContext } from "react";
-import { Circle, Center} from "@chakra-ui/react";
+import { Circle, Center, useToast} from "@chakra-ui/react";
 import { ComEthContext } from "../../context/ComEthContext";
+import { Web3Context } from "web3-hooks";
 
 const AddUserAndPay = () => {
+  const [web3State] = useContext(Web3Context);
   const comEth = useContext(ComEthContext);
+  const toast = useToast();
+
 
   const handleAddUser = async () => {
     try {
       await comEth.addUser();
-      console.log("you have been add !");
+      toast({
+        title: "Vous êtes un utilisateur de ComEth",
+        description: `Félicitation, bienvenue dans votre ComEth`, // hash de la transac
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
     } catch (e) {
+      if (e.code === 4001) {
+        toast({
+          title: "Transaction refusé à la signature",
+          description: e.message,
+          status: "error",
+          duration: 4000,
+          isClosable: true,
+        });
+      }
       console.log(e.error);
     }
   };
   const handlePay = async () => {
     try {
-      await comEth.pay();
-      console.log("you have been pay !");
+
+      const get = await comEth.getAmountToBePaid(web3State.account)
+      await comEth.pay({value : get._hex});
+      toast({
+        title: "Vous êtes régularisé",
+        description: `Transaction hash: ${get.hash}`, // hash de la transac
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
     } catch (e) {
+      if (e.code === 4001) {
+        toast({
+          title: "Transaction refusé à la signature",
+          description: e.message,
+          status: "error",
+          duration: 4000,
+          isClosable: true,
+        });
+      }
       console.log(e.error);
     }
   };
--- src/components/organisms/HomeBody.js
+++ src/components/organisms/HomeBody.js
@@ -12,18 +12,18 @@ const HomeBody = () => {
     <AddUserAndPay />
       <SimpleGrid columns={[1, 1, 2, 3]} spacing="40px" mr={{base:"1rem",sm:"3rem"}} mt="4rem" ml={{base:"1rem", sm:"3rem", md:"10rem", lg:"12rem"}}>
         <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"20rem", md:"22rem", lg:"28rem"}}>
-          <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{sm:"9rem", md:"9rem", lg:"17rem"}}>
+          <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"5rem",sm:"6rem", md:"8rem", lg:"15rem"}}>
            Venez voter les propositions déjà suggérer par votre groupe ComEth
            </Box>
           <VoteButton />
         </Box>
         <Box rounded="md"  textAlign="center" p="2rem"  backgroundColor="blackAlpha.200" height={{sm:"20rem", md:"22rem", lg:"28rem"}}>
-        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{sm:"9rem", md:"9rem", lg:"17rem"}}>
+        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"5rem",sm:"6rem", md:"8rem", lg:"15rem"}}>
           Soumettez vos propres propositions à faire voter à votre groupe
           </Box> <ProposButton />
         </Box>
         <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"20rem", md:"22rem", lg:"28rem"}}>
-        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{sm:"9rem", md:"9rem", lg:"17rem"}}>
+        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"5rem",sm:"6rem", md:"8rem", lg:"15rem"}}>
           Retrouvez vos informations, statistique, proposition déjà voter ici
           </Box>
           <InfoButton />
