392073d41dbb1c50cacf29c67cf4fbae8d80642c
StellaGreen <vickysoler5@gmail.com>
2021-09-13 19:26:47 +0200

    front body home and info

--- src/components/organisms/HomeBody.js
+++ src/components/organisms/HomeBody.js
@@ -11,19 +11,19 @@ const HomeBody = () => {
     <>
     <AddUserAndPay />
       <SimpleGrid columns={[1, 1, 2, 3]} spacing="40px" mr={{base:"1rem",sm:"3rem"}} mt={{ base: "1rem", sm: "2rem", md: "2rem", lg: "2rem" }} ml={{base:"1rem", sm:"3rem", md:"10rem", lg:"12rem"}}>
-        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"20rem", md:"22rem", lg:"28rem"}}>
-          <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"3rem",sm:"4rem", md:"8rem", lg:"12rem"}}>
+        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"35%", md:"55%", lg:"90%"}}>
+          <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"25%",sm:"25%"}}>
            Venez voter les propositions déjà suggérer par votre groupe ComEth
            </Box>
           <VoteButton />
         </Box>
-        <Box rounded="md"  textAlign="center" p="2rem"  backgroundColor="blackAlpha.200" height={{sm:"20rem", md:"22rem", lg:"28rem"}}>
-        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"3rem",sm:"4rem", md:"6rem", lg:"11rem"}}>
+        <Box rounded="md"  textAlign="center" p="2rem"  backgroundColor="blackAlpha.200" height={{sm:"35%", md:"55%", lg:"90%"}}>
+        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"25%",sm:"25%"}}>
           Soumettez vos propres propositions à faire voter à votre groupe
           </Box> <ProposButton />
         </Box>
-        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"20rem", md:"22rem", lg:"28rem"}}>
-        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"3rem",sm:"4rem", md:"6rem", lg:"11rem"}}>
+        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"35%", md:"55%", lg:"90%"}}>
+        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="teal.400" mb={{base:"25%",sm:"25%"}}>
           Retrouvez vos informations, statistique, proposition déjà voter ici
           </Box>
           <InfoButton />
