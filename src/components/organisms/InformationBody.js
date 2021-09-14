3d0b0a2e7165e653318f8751c2d0d6397e0896a1
StellaGreen <vickysoler5@gmail.com>
2021-09-14 20:02:45 +0200

    correction Box front

--- src/components/organisms/InformationBody.js
+++ src/components/organisms/InformationBody.js
@@ -7,27 +7,54 @@ import StatsButton from "../atoms/StatsButton";
 const InformationBody = () => {
   return (
     <>
-    
-    <SimpleGrid columns={[1, 1, 2, 3]} spacing="40px" mr={{base:"1rem",sm:"3rem"}} mt={{ base: "1rem", sm: "2rem", md: "2rem", lg: "2rem" }} ml={{base:"1rem", sm:"3rem", md:"10rem", lg:"12rem"}}>
-    <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"35%", md:"55%", lg:"90%"}}>
-    <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="#2ac9c7" mb={{base:"25%",sm:"25%"}}>
-           Ici sont les proposition déjà voter avec leurs paramettres
-           </Box>
+      <SimpleGrid
+        columns={[1, 1, 2, 3]}
+        spacing="40px"
+        mr={{ base: "1rem", sm: "3rem" }}
+        mt={{ base: "1rem", sm: "2rem", md: "2rem", lg: "2rem" }}
+        ml={{ base: "1rem", sm: "3rem", md: "10rem", lg: "12rem" }}
+      >
+        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height="90%">
+          <Box
+            fontWeight="bold"
+            boxShadow="inner"
+            p="0.5rem"
+            rounded="md"
+            backgroundColor="#2ac9c7"
+            mb={{ base: "25%", sm: "25%" }}
+          >
+            Ici sont les proposition déjà voter avec leurs paramettres
+          </Box>
           <StatsButton />
         </Box>
-        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"35%", md:"55%", lg:"90%"}}>
-        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="#2ac9c7" mb={{base:"25%",sm:"25%"}}>
-           Ici c'est pour voir les budgets utilisé et totaux
-           </Box> <BudgetButton />
+        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height="90%">
+          <Box
+            fontWeight="bold"
+            boxShadow="inner"
+            p="0.5rem"
+            rounded="md"
+            backgroundColor="#2ac9c7"
+            mb={{ base: "25%", sm: "25%" }}
+          >
+            Ici c'est pour voir les budgets utilisé et totaux
+          </Box>{" "}
+          <BudgetButton />
         </Box>
-        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height={{sm:"35%", md:"55%", lg:"90%"}}>
-        <Box fontWeight="bold" boxShadow="inner" p="0.5rem" rounded="md" backgroundColor="#2ac9c7" mb={{base:"25%",sm:"25%"}}>
-           Ici c'est pour voir qui est actif, qui est banni, qui fait partie de ta ComEth
-           </Box>
+        <Box rounded="md" textAlign="center" p="2rem" backgroundColor="blackAlpha.200" height="90%">
+          <Box
+            fontWeight="bold"
+            boxShadow="inner"
+            p="0.5rem"
+            rounded="md"
+            backgroundColor="#2ac9c7"
+            mb={{ base: "25%", sm: "25%" }}
+          >
+            Ici c'est pour voir qui est actif, qui est banni, qui fait partie de
+            ta ComEth
+          </Box>
           <PeopleButton />
         </Box>
       </SimpleGrid>
-      
     </>
   );
 };
