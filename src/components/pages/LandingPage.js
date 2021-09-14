060e2fd801290790748dbf7f586836433643ac4a
StellaGreen <vickysoler5@gmail.com>
2021-09-14 16:25:31 +0200

    reglage vh landing

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <>
     {/* TODO : faire la page front joulie  */}
-      <Box minH="100vh" maxH="100%" w="100%" bgGradient="linear(to-t,blue.100, blue.400);">
+      <Box w="100%" minH="100vh" bgGradient="linear(to-t,blue.100, blue.400);">
         <LandingPageTemplate />
       </Box>
     </>
