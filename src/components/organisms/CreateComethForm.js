9a94d8927bca884e76aa78749f891a9f3a58339b
StellaGreen <vickysoler5@gmail.com>
2021-09-16 16:04:43 +0200

    ok

--- src/components/organisms/CreateComethForm.js
+++ src/components/organisms/CreateComethForm.js
@@ -1,15 +1,23 @@
 import { React, useEffect, useState } from "react";
-import { Box, Circle, useToast, Input, Center, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  Circle,
+  useToast,
+  Input,
+  Center,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 import { Redirect } from "react-router-dom";
 
 import { ComEthFactoryContext } from "../../App";
 import { ComEthAddressContext } from "../../App";
 import { useContext } from "react";
 import { Web3Context } from "web3-hooks";
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 const CreateComethForm = () => {
-  const [created , setCreated ] = useState(false)
+  const [created, setCreated] = useState(false);
   const [web3State] = useContext(Web3Context);
   const comEthFactory = useContext(ComEthFactoryContext);
   const { comEthAddress, setComEthAddress } = useContext(ComEthAddressContext);
@@ -19,12 +27,14 @@ const CreateComethForm = () => {
 
   const handleChangeSubscription = (e) => {
     setSubscriptionPrice(e.target.value.toString());
-    console.log("ETH",e.target.value)
+    console.log("ETH", e.target.value);
   };
 
   const handleClickCreate = async () => {
     try {
-      let tx = await comEthFactory.createComEth(ethers.utils.parseEther(subscriptionPrice));
+      let tx = await comEthFactory.createComEth(
+        ethers.utils.parseEther(subscriptionPrice)
+      );
       await tx.wait();
       toast({
         title: "Transaction confirmée",
@@ -44,23 +54,22 @@ const CreateComethForm = () => {
         });
       }
       console.log(e);
-      
     }
   };
   useEffect(() => {
     // si simpleStorage est pas null alors
     if (comEthFactory) {
       const cb = (ComEthAddress) => {
-          setComEthAddress(ComEthAddress);
-          localStorage.setItem("AddressComEth", JSON.stringify(ComEthAddress))
-          toast({
-            title: "Votre communauté a sa propre addresse Ethereum !",
-            description: `L'addresse de votre communauté : ${ComEthAddress}`,
-            status: "info",
-            position: "top-right",
-            duration: 9000,
-            isClosable: true,
-          });
+        setComEthAddress(ComEthAddress);
+        localStorage.setItem("AddressComEth", JSON.stringify(ComEthAddress));
+        toast({
+          title: "Votre communauté a sa propre addresse Ethereum !",
+          description: `L'addresse de votre communauté : ${ComEthAddress}`,
+          status: "info",
+          position: "top-right",
+          duration: 9000,
+          isClosable: true,
+        });
       };
       // ecouter sur l'event DataSet
       comEthFactory.on("ComEthCreated", cb);
@@ -111,36 +120,40 @@ const CreateComethForm = () => {
             mt="3rem"
             rounded="md"
             backgroundColor="teal.400"
-          ><Text textAlign="center" mb={{base:"0.3rem", sm:"0"}}>
-            Montant de la souscription
+          >
+            <Text textAlign="center" mb={{ base: "0.3rem", sm: "0" }}>
+              Montant de la souscription
             </Text>
-            <Tooltip rounded="md" label="Faites attention de bien mettre un point et non une virgule" fontSize="md">
             <Input
               onChange={handleChangeSubscription}
               ml="2rem"
               mr="0.5rem"
               w="25%"
-              placeholder="10"
-            /></Tooltip>
-            ETH
+              placeholder="0.1"
+            />
+            <Text>ETH</Text>
           </Box>
         </Center>
-        <Tooltip rounded="md" label="Une fois que vous aurez validé, attendez le pop-up, vous serez redirigé automatiquement dans l'application" fontSize="md">
-        <Circle
-          fontWeight="bold"
-          textAlign="center"
-          fontSize={{ base: "md", sm: "lg" }}
-          backgroundColor="whiteAlpha.400"
-          boxShadow="lg"
-          onClick={handleClickCreate}
-          _hover={{ bg: "#0db5aa" }}
-          _active={{ bg:"#8deaf2"}}
-          _selected={{ bg: "#17d4c7" }}
-          p="0.5em"
-          margin="2rem"
+        <Tooltip
+          rounded="md"
+          label="Une fois que vous aurez validé, attendez le pop-up, vous serez redirigé automatiquement dans l'application"
+          fontSize="md"
         >
-          Créez votre ComEth
-        </Circle>
+          <Circle
+            fontWeight="bold"
+            textAlign="center"
+            fontSize={{ base: "md", sm: "lg" }}
+            backgroundColor="whiteAlpha.400"
+            boxShadow="lg"
+            onClick={handleClickCreate}
+            _hover={{ bg: "#0db5aa" }}
+            _active={{ bg: "#8deaf2" }}
+            _selected={{ bg: "#17d4c7" }}
+            p="0.5em"
+            margin="2rem"
+          >
+            Créez votre ComEth
+          </Circle>
         </Tooltip>
       </Box>
       {created && <Redirect exact from="/create" to="/home" />}
